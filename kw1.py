52395d3b049cd32562b394e816878c85cb7189ab
Xyan <xyan@gmx.net>
2023-12-02 22:50:51 +0000

    Update with webserver

--- kw1.py
+++ kw1.py
@@ -4,8 +4,7 @@
 import binascii
 import logging
 import logging.handlers
-import time
-from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from threading import Thread
 
 import paho.mqtt.client as mqtt
@@ -14,15 +13,25 @@ import serial
 logging.getLogger().setLevel('DEBUG')
 logging.info('Starting Viessmann2mqtt')
 
-MQTT_USER = "Z"
-MQTT_PASSWORD = "Y"
-MQTT_SERVER = 'X'
+MQTT_USER = "clientabc123"
+MQTT_PASSWORD = "publicabc123"
+MQTT_SERVER = '3.73.129.80'
 MQTT_TOPIC = 'Viessmann/'  # should end with /
 hostName = ""
-serverPort = 80
+serverPort = 443
 
 CMD_VREAD = binascii.unhexlify('F7')
 CMD_VWRITE = binascii.unhexlify('F4')
+
+STYLE = '''
+    div {
+    }
+    div span {
+        font-weight: bold;
+    }
+'''
+
+WRITE_COMMAND = None
 
 
 def connecthandler(mqc, userdata, flags, rc):
@@ -36,15 +45,33 @@ def disconnecthandler(mqc, userdata, rc):
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>KW1</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        for cmd in commands:
-            self.wfile.write(bytes("<div>Value %s: %s</div>" % (cmd.name, cmd.res), "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        global WRITE_COMMAND
+
+        if self.path != '/':
+            try:
+                WRITE_COMMAND = write_commands[self.path]
+            except:
+                pass
+            self.send_response(307)
+            self.send_header("Location", "/")
+            self.end_headers()
+        else:
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("<html>"
+                                   "<head>"
+                                   "<title>KW1</title>"
+                                   "<meta http-equiv='refresh' content='30'>"
+                                   "<style>%s</style>"
+                                   "</head>\n" % STYLE, "utf-8"))
+            self.wfile.write(bytes("<p>Request: %s</p>\n" % self.path, "utf-8"))
+            self.wfile.write(bytes("<body>\n", "utf-8"))
+            self.wfile.write(bytes("<div>Partymodus <a href='/party_on'>AN</a>|<a href='/party_off'>AUS</a></div>\n", "utf-8"))
+            self.wfile.write(bytes("<div>Modus <a href='/mod_ww'>Warm Wasser</a>|<a href='/mod_all'>Alles An</a></div>\n", "utf-8"))
+            for cmd in stat_commands:
+                self.wfile.write(bytes("<div>%s: <span>%s</span></div>\n" % (cmd.desc, cmd.res), "utf-8"))
+            self.wfile.write(bytes("</body></html>\n", "utf-8"))
 
 
 def startServer():
@@ -84,13 +111,14 @@ def startLoop():
     print("Connected")
 
     while True:
-        loop(ser, commands, mqc)
+        loop(ser, stat_commands, mqc)
         ser.timeout = 5
         ser.read(100)
     # print("Done!")
 
 
 def loop(ser, commands, mqc):
+    global WRITE_COMMAND
     print("Waiting for ACK")
     ser.timeout = 3
 
@@ -100,6 +128,9 @@ def loop(ser, commands, mqc):
             ser.write(binascii.unhexlify('01'))
             for cmd in commands:
                 command(ser, cmd)
+            if WRITE_COMMAND is not None:
+                command(ser, WRITE_COMMAND)
+                WRITE_COMMAND = None
     except Exception as e:
         logging.error("Unhandled error serial [" + str(e) + "]")
 
@@ -119,13 +150,20 @@ def command(ser, cmd):
     # print("Proto", cmd.protocmd)
     ser.write(cmd.address)
     # print("Address", cmd.address)
-    ser.timeout = 1
     ser.write(cmd.length.to_bytes(1, 'big'))
-    # print("Now reading", cmd.length)
-    val = ser.read(cmd.length)
-    # print('OptoLink < %s' % binascii.hexlify(val))
-    cmd.res = convertunit(cmd.unit, val)
-    print('Command %s returned %s' % (cmd.name, cmd.res))
+    ser.timeout = 1
+    if cmd.protocmd == CMD_VREAD:
+        # print("Now reading", cmd.length)
+        val = ser.read(cmd.length)
+        # print('OptoLink < %s' % binascii.hexlify(val))
+        cmd.res = convertunit(cmd.unit, val)
+        # print('Command %s returned %s' % (cmd.name, cmd.res))
+    elif cmd.protocmd == CMD_VWRITE:
+        ser.write(cmd.res)
+        if ser.read(1) == binascii.unhexlify('00'):
+            pass # print('Command %s wrote %s' % (cmd.name, cmd.res))
+        else:
+            pass # print('Error in write command %s %s' % (cmd.name, cmd.res))
 
 
 def errorcode(errorcode):
@@ -271,49 +309,64 @@ def convertunit(unit, value):
         if value == binascii.unhexlify('03'):
             return 'Heizung und Warm Wasser'
         if value == binascii.unhexlify('04'):
-            return 'Heizung und Warm Wasser'
+            return 'Heizung und Warmwasser'
         if value == binascii.unhexlify('05'):
             return 'Abgeschaltet'
     return 'error'
 
 
 class Command:
-    res = 'NA'
-
-    def __init__(self, name, protocmd, address, length, unit):
+    def __init__(self, name, protocmd, address, length, unit, desc, res='NA'):
         self.name = name
         self.protocmd = protocmd
         self.address = binascii.unhexlify(address)
         self.length = length
         self.unit = unit
+        self.desc = desc
+        self.res = res
 
 
-commands = [
+write_commands = {
+    "/party_on": Command("setBetriebPartyM1", CMD_VWRITE, '2303', 1, 'RT', 'Partymodus', binascii.unhexlify('01')),
+    "/party_off": Command("setBetriebPartyM1", CMD_VWRITE, '2303', 1, 'RT', 'Partymodus', binascii.unhexlify('00')),
+    "/mod_ww": Command("setBetriebsArt", CMD_VWRITE, '2301', 1, 'BA', 'Modus', binascii.unhexlify('00')),
+    "/mod_all": Command("setBetriebsArt", CMD_VWRITE, '2301', 1, 'BA', 'Modus', binascii.unhexlify('04')),
+    "/ww_60": Command("setTempWWsoll", CMD_VWRITE, '6300', 1, 'ST', 'Warmwasser Soll 60', int.to_bytes(60,2,"little")),
+    "/ww_55": Command("setTempWWsoll", CMD_VWRITE, '6300', 1, 'ST', 'Warmwasser Soll 55', int.to_bytes(55,2,"little")),
+    "/ww_50": Command("setTempWWsoll", CMD_VWRITE, '6300', 1, 'ST', 'Warmwasser Soll 50', int.to_bytes(50,2,"little")),
+    "/ww_45": Command("setTempWWsoll", CMD_VWRITE, '6300', 1, 'ST', 'Warmwasser Soll 45', int.to_bytes(45,2,"little")),
+}
+
+stat_commands = [
     # Command("deviceType",CMD_VREAD,'00F8',2,'deviceType'),
-    Command("getTempA", CMD_VREAD, '0800', 2, 'UT'),  # out temperature
-    Command("getTempWWist", CMD_VREAD, '0804', 2, 'UT'),  # warm water temperature
-    Command("getTempKist", CMD_VREAD, '0802', 2, 'UT'),  # kessel temperature
-    Command("getTempWWsoll", CMD_VREAD, '6300', 1, 'ST'),  # warm water target
-    Command("getTempRaumsoll", CMD_VREAD, '2306', 1, 'ST'),  # room temperature target
-    Command("getTempKsoll", CMD_VREAD, '5502', 2, 'UT'),  # kessel target
-    Command("getBrennerStatus", CMD_VREAD, '551E', 1, 'RT'),  # burner state
-    Command("getBrennerStarts", CMD_VREAD, '088A', 2, 'CO'),  # burner starts
-    Command("getBrennerStunden1", CMD_VREAD, '08A7', 4, 'CS'),  # burner hours lvl 1
-    Command("getBrennerStunden2", CMD_VREAD, '08AB', 4, 'CS'),  # burner hours lvl 2
-    Command("getPumpeStatusM1", CMD_VREAD, '2906', 1, 'RT'),  # heater pump
-    Command("getStatusStoerung", CMD_VREAD, '7579', 1, 'RT'),  # errors
+    Command("getBetriebsArt", CMD_VREAD, '2301', 1, 'BA', 'Modus'),  # operation mode
+    Command("getBetriebPartyM1", CMD_VREAD, '2303', 1, 'RT', 'Partymodus'),  # operation mode
+    # Command("getBetriebPartyM2", CMD_VREAD, '3303', 1, 'RT', 'Partymodus M2'),  # operation mode
+    Command("getTempPartyM1", CMD_VREAD, '2308', 1, 'ST', 'Partytemperatur'),  # operation mode
+    # Command("getTempPartyM2", CMD_VREAD, '3308', 1, 'ST', 'Partytemperatur M2'),  # operation mode
+    Command("getTempA", CMD_VREAD, '0800', 2, 'UT', 'Aussentemperatur'),  # out temperature
+    Command("getTempWWist", CMD_VREAD, '0804', 2, 'UT', 'Warmwasser Ist'),  # warm water temperature
+    Command("getTempWWsoll", CMD_VREAD, '6300', 1, 'ST', 'Warmwasser Soll'),  # warm water target
+    Command("getTempKist", CMD_VREAD, '0802', 2, 'UT', 'Heizkessel Ist'),  # kessel temperature
+    Command("getTempKsoll", CMD_VREAD, '5502', 2, 'UT', 'Heizkessel Soll'),  # kessel target
+    Command("getTempRaumsoll", CMD_VREAD, '2306', 1, 'ST', 'Raumsoll'),  # room temperature target
+    Command("getBrennerStatus", CMD_VREAD, '551E', 1, 'RT', 'Brennerstatus'),  # burner state
+    Command("getBrennerStarts", CMD_VREAD, '088A', 2, 'CO', 'Brenner Starts'),  # burner starts
+    Command("getBrennerStunden1", CMD_VREAD, '08A7', 4, 'CS', 'Brennstunden I'),  # burner hours lvl 1
+    Command("getBrennerStunden2", CMD_VREAD, '08AB', 4, 'CS', 'Brennstunden II'),  # burner hours lvl 2
+    Command("getPumpeStatusM1", CMD_VREAD, '2906', 1, 'RT', 'Heizpumpe'),  # heater pump
+    Command("getPumpeTankStatus", CMD_VREAD, '0845', 1, 'RT', 'Speicherpumpe'),  # store/collector pump
+    Command("getPumpeZStatus", CMD_VREAD, '0846', 1, 'RT', 'Zirkulationspumpe'),  # zirkulation pump
+    Command("getStatusStoerung", CMD_VREAD, '7579', 1, 'RT', 'Stoerung'),  # errors
+    Command("getBrennerStoerung", CMD_VREAD, '0883', 1, 'RT', 'Brenner Fehler'),  # burner errors
 
-    Command("getExhaustTemp", CMD_VREAD, '0808', 2, 'UT'),  # abgas temp
-    Command("getBackwardTemp", CMD_VREAD, '080A', 2, 'UT'),  # ruecklauf temp
-    Command("getBackwardM2Temp", CMD_VREAD, '3902', 2, 'UT'),  # ruecklauf temp
-    Command("getForwardTemp", CMD_VREAD, '080C', 2, 'UT'),  # vorlauf temp
-    Command("getForwardM2Temp", CMD_VREAD, '3900', 2, 'UT'),  # vorlauf temp
-    Command("getExhaustLowTemp", CMD_VREAD, '5525', 2, 'UT'),  # exhaust low temp
-    Command("getTempAAvr", CMD_VREAD, '5527', 2, 'UT'),  # exhaust avr temp
-    Command("getBrennerStoerung", CMD_VREAD, '0883', 1, 'RT'),  # burner errors
-    Command("getPumpeTankStatus", CMD_VREAD, '0845', 1, 'RT'),  # store/collector pump
-    Command("getPumpeZStatus", CMD_VREAD, '0846', 1, 'RT'),  # zirkulation pump
-    Command("getBetriebsArt", CMD_VREAD, '2301', 1, 'BA'),  # operation mode
+    # Command("getExhaustTemp", CMD_VREAD, '0808', 2, 'UT'),  # abgas temp
+    # Command("getBackwardTemp", CMD_VREAD, '080A', 2, 'UT'),  # ruecklauf temp
+    # Command("getBackwardM2Temp", CMD_VREAD, '3902', 2, 'UT'),  # ruecklauf temp
+    # Command("getForwardTemp", CMD_VREAD, '080C', 2, 'UT'),  # vorlauf temp
+    # Command("getForwardM2Temp", CMD_VREAD, '3900', 2, 'UT'),  # vorlauf temp
+    # Command("getExhaustLowTemp", CMD_VREAD, '5525', 2, 'UT'),  # exhaust low temp
+    # Command("getTempAAvr", CMD_VREAD, '5527', 2, 'UT'),  # exhaust avr temp
 
 ]
 
